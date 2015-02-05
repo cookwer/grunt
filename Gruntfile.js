6ac18b84cde80f2f9d219afc4fc85081e50c61b6
Andrey Surkov <cookwer@gmail.com>
2015-02-05 23:24:19 +0300

    no message

--- Gruntfile.js
+++ Gruntfile.js
@@ -128,9 +128,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-newer');
 
     grunt.registerTask('build', ['concat', 'uglify']);
-    grunt.registerTask('default', ['newer:watch']);
-    grunt.registerTask('minify', ['newer:concat:all']);
+    grunt.registerTask('default', ['watch']);
 };
